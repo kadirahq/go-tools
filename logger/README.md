1e96883317374fb8424d7fc4f3b611dc4136449c
Muhammed Thanish <mnmtanish@gmail.com>
2015-08-25 16:07:40 +0530

    Refactor logger

---
+++ logger/README.md
@@ -0,0 +1,45 @@
+# Logger
+
+A basic logger package which supports multiple levels of logging. Users can enable of disable log levels using the "log" environment variable. By default, logger logs **info** and **error** logs. Custom log levels can be added as required. More information can be found on [godoc](http://godoc.org/github.com/kadirahq/go-tools/logger).
+
+
+
+## Installing
+
+``` shell
+go get -u github.com/kadirahq/go-tools/logger
+```
+
+
+
+## Examples
+
+``` go
+package main
+
+import (
+	"github.com/go-errors/errors"
+	"github.com/kadirahq/go-tools/logger"
+)
+
+func main() {
+	// create a new logger
+	log := logger.New("webapp")
+	log.Info("starting application on port:", 8080)
+
+	// Error stack traces will be printed when the go-errors/errors
+	// package was used to create (or wrap) the error.
+	err := errors.New("error with stacktrace")
+	log.Error(err, "foo =>", "bar")
+
+	// debug level is not enabled by default
+	// use logger.Enable("debug") or the env variable
+	// ex. log="info,error,debug" go run main.go
+	log.Debug("debug options", []int{1, 2, 3, 4, 5})
+
+	// any number of custom log levels can be used
+	// use logger.Enable("lvl9001") or the env variable
+	// ex. log="info,error,lvl9001" go run main.go
+	log.Print("lvl9001", "!!!")
+}
+```
--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,1 +1,104 @@
 package logger
+
+import (
+	"bytes"
+	"errors"
+	"strings"
+	"testing"
+
+	goerr "github.com/go-errors/errors"
+)
+
+var (
+	buffer = bytes.NewBuffer(nil)
+)
+
+func init() {
+	output.SetOutput(buffer)
+	output.SetFlags(0)
+}
+
+func TestDefaults(t *testing.T) {
+	if !levels["info"] || !levels["error"] {
+		t.Fatal("Default log levels aren't enabled")
+	}
+}
+
+func TestPrint(t *testing.T) {
+	buffer.Reset()
+
+	// should not print
+	Print("test", 0, 0)
+
+	// enable log level "test"
+	Enable("test")
+
+	// should print
+	Print("test", 1, 2)
+	Print("test", 4, 5, "hello")
+
+	// Disable log level "test"
+	Disable("test")
+
+	// should not print
+	Print("test", 6, 7)
+
+	// expected output
+	exp := "(test) app: [1 2]\n" +
+		"(test) app: [4 5 hello]\n"
+
+	if got := string(buffer.Bytes()); got != exp {
+		t.Fatalf("exp: %s got: %s", exp, got)
+	}
+}
+
+func TestInfo(t *testing.T) {
+	buffer.Reset()
+
+	Info(1, 2, 3)
+
+	exp := colblu("(info) app") + ": [1 2 3]\n"
+
+	if got := string(buffer.Bytes()); got != exp {
+		t.Fatalf("exp: %s got: %s", exp, got)
+	}
+}
+
+func TestDebug(t *testing.T) {
+	buffer.Reset()
+
+	Debug(1, 2, 3)
+
+	exp := colyel("(debug) app") + ": [1 2 3]\n"
+
+	if got := string(buffer.Bytes()); got != exp {
+		t.Fatalf("exp: %s got: %s", exp, got)
+	}
+}
+
+func TestError(t *testing.T) {
+	buffer.Reset()
+
+	Error(errors.New("test error"), 1, 2, 3)
+
+	exp := colred("(error) app") + ": [1 2 3]\n" +
+		"test error\n"
+
+	if got := string(buffer.Bytes()); got != exp {
+		t.Fatalf("exp: %s got: %s", exp, got)
+	}
+}
+
+func TestErrorWithStack(t *testing.T) {
+	buffer.Reset()
+
+	Error(goerr.New("test error"), 1, 2, 3)
+
+	exp := colred("(error) app") + ": [1 2 3]\n" +
+		"*errors.errorString test error\n"
+
+	if got := string(buffer.Bytes()); !strings.HasPrefix(got, exp) &&
+		len(got) > len(exp) {
+		t.Fatalf("exp: %s got: %s", exp, got)
+	}
+}
