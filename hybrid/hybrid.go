a86a6731fb2934eb4bdff1e9b30526c8a45f6f12
Muhammed Thanish <mnmtanish@gmail.com>
2015-09-08 10:26:34 +0530

    Add Read methods

--- hybrid/hybrid.go
+++ hybrid/hybrid.go
@@ -18,3 +18,17 @@ func NewUint32(v uint32) *Uint32 {
 
 	return &Uint32{vptr, data}
 }
+
+func ReadUint32(d []byte) *Uint32 {
+	data := d[:4]
+	head := (*reflect.SliceHeader)(unsafe.Pointer(&data))
+	vptr := (*uint32)(unsafe.Pointer(head.Data))
+
+	return &Uint32{vptr, data}
+}
+
+func (s *Uint32) Read(d []byte) {
+	head := (*reflect.SliceHeader)(unsafe.Pointer(&d))
+	s.Value = (*uint32)(unsafe.Pointer(head.Data))
+	s.Bytes = d[:4]
+}
--- hybrid/hybrid_test.go
+++ hybrid/hybrid_test.go
@@ -2,6 +2,7 @@ package hybrid
 
 import (
 	"bytes"
+	"encoding/binary"
 	"testing"
 )
 
@@ -20,5 +21,51 @@ func TestUint32(t *testing.T) {
 	*s.Value = 256
 	if !bytes.Equal(s.Bytes, []byte{0, 1, 0, 0}) || *s.Value != 256 {
 		t.Fatal("wrong value")
+	}
+
+	s = ReadUint32([]byte{1, 1, 0, 0, 0, 0, 0, 0})
+	if !bytes.Equal(s.Bytes, []byte{1, 1, 0, 0}) || *s.Value != 257 {
+		t.Fatal("wrong value")
+	}
+
+	s.Read([]byte{2, 1, 0, 0, 0, 0, 0, 0})
+	if !bytes.Equal(s.Bytes, []byte{2, 1, 0, 0}) || *s.Value != 258 {
+		t.Fatal("wrong value")
+	}
+}
+
+func BenchmarkBinaryRead(b *testing.B) {
+	var v uint32
+	var d = make([]byte, b.N*4)
+	var s = bytes.NewBuffer(d)
+
+	for i := 0; i < b.N; i++ {
+		binary.Read(s, binary.LittleEndian, v)
+	}
+}
+
+func BenchmarkUint32Read(b *testing.B) {
+	var d = make([]byte, b.N*4)
+	var s = NewUint32(0)
+
+	for i := 0; i < b.N; i++ {
+		s.Read(d[i*4:])
+	}
+}
+
+func BenchmarkBinaryWrite(b *testing.B) {
+	var v uint32
+	var d = make([]byte, b.N*4)
+	var s = bytes.NewBuffer(d)
+
+	for i := 0; i < b.N; i++ {
+		binary.Write(s, binary.LittleEndian, v)
+	}
+}
+
+func BenchmarkUint32Write(b *testing.B) {
+	var s = NewUint32(0)
+	for i := 0; i < b.N; i++ {
+		*s.Value = uint32(i)
 	}
 }
