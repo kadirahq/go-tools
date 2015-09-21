2025bf9c4cc75a7e76d8c24145f859ac85b136b6
Muhammed Thanish <mnmtanish@gmail.com>
2015-09-21 10:12:29 +0530

    Add memory map packages

---
+++ memmap/memmap.go
@@ -0,0 +1,119 @@
+package memmap
+
+import (
+	"errors"
+	"os"
+	"reflect"
+	"syscall"
+	"unsafe"
+)
+
+const (
+	fmode = os.O_RDWR | os.O_CREATE
+	fperm = 0644
+	mflag = syscall.MAP_SHARED
+	mprot = syscall.PROT_READ | syscall.PROT_WRITE
+	msync = syscall.MS_SYNC
+)
+
+var (
+	// ErrZeroSz is used when the user attempts to create a memory map
+	// with an empty file. Use the size parameter to set required size.
+	ErrZeroSz = errors.New("cannot create mmap with empty file")
+)
+
+// Map is a struct which abstracts memory map system calls and provides a fast
+// and easy to use api. The Map should be unmapped when not in use.
+type Map struct {
+	Data []byte
+	file *os.File
+	hlen uintptr
+	hadr uintptr
+}
+
+// NewMap creates a new memory map struct. This struct contains map data and
+// other information needed to manipulate the memory map later.
+func NewMap(path string, size int64) (m *Map, err error) {
+	if size == 0 {
+		return nil, ErrZeroSz
+	}
+
+	file, err := os.OpenFile(path, fmode, fperm)
+	if err != nil {
+		return nil, err
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		file.Close()
+		return nil, err
+	}
+
+	fd := file.Fd()
+	sz := info.Size()
+
+	if sz != size {
+		if err := file.Truncate(size); err != nil {
+			file.Close()
+			return nil, err
+		}
+
+		sz = size
+	}
+
+	var from int64
+	data, err := syscall.Mmap(int(fd), from, int(sz), mprot, mflag)
+	if err != nil {
+		file.Close()
+		return nil, err
+	}
+
+	// get slice header to get memory address and length
+	head := (*reflect.SliceHeader)(unsafe.Pointer(&data))
+
+	m = &Map{
+		Data: data,
+		file: file,
+		hadr: head.Data,
+		hlen: uintptr(head.Len),
+	}
+
+	return m, nil
+}
+
+// Lock loads all memory pages in physical memory. This can take a long time for
+// larger files but access to these memory locations will be faster.
+func (m *Map) Lock() (err error) {
+	if err := syscall.Mlock(m.Data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Sync synchronizes the memory map with the mapped file. This can be used to
+// ensure that all data is written to the disk successfully. Calling the Sync
+// method is necessary to survive OS kernel level panics and crashes.
+func (m *Map) Sync() (err error) {
+	_, _, errno := syscall.Syscall(syscall.SYS_MSYNC, m.hadr, m.hlen, msync)
+	if errno != 0 {
+		err := syscall.Errno(errno)
+		return err
+	}
+
+	return nil
+}
+
+// Close unmaps data and closes the file handler. Changes done to the memory
+// map will be synced to the disk before closing to prevent data loss.
+func (m *Map) Close() (err error) {
+	if err := m.Sync(); err != nil {
+		return err
+	}
+
+	if err := syscall.Munmap(m.Data); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ segmmap/segmmap.go
@@ -0,0 +1,277 @@
+package segmmap
+
+import (
+	"errors"
+	"io/ioutil"
+	"path"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/kadirahq/go-tools/memmap"
+)
+
+var (
+	// ErrZeroSz is used when the user attempts to create a memory map
+	// with an empty file. Use the size parameter to set required size.
+	ErrZeroSz = errors.New("cannot create mmap with empty file")
+)
+
+// Map is a collection of memory maps. Using a set of memory mapped files can
+// be faster than using a single memory map file. Also, it allocates faster.
+type Map struct {
+	Maps map[int64]*memmap.Map
+
+	path string
+	size int64
+	mutx *sync.RWMutex
+}
+
+// NewMap creates a collection of memory maps on given path
+func NewMap(path string, size int64) (m *Map, err error) {
+	if size == 0 {
+		return nil, ErrZeroSz
+	}
+
+	m = &Map{
+		Maps: map[int64]*memmap.Map{},
+		path: path,
+		size: size,
+		mutx: &sync.RWMutex{},
+	}
+
+	return m, nil
+}
+
+// Load loads a segment file into memory.
+func (m *Map) Load(id int64) (f *memmap.Map, err error) {
+	// fast path: file already exists
+	// RLocks costs lower than Locks
+	m.mutx.RLock()
+	f, ok := m.Maps[id]
+	m.mutx.RUnlock()
+
+	if ok {
+		return f, nil
+	}
+
+	m.mutx.Lock()
+	f, err = m.load(id)
+	if err != nil {
+		m.mutx.Unlock()
+		return nil, err
+	}
+
+	m.mutx.Unlock()
+	return f, nil
+}
+
+// LoadAll loads all existing segment files into memory.
+func (m *Map) LoadAll() (err error) {
+	dir := path.Dir(m.path)
+	base := path.Base(m.path)
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		name := file.Name()
+		if strings.HasPrefix(name, base) {
+			idstr := strings.TrimPrefix(name, base)
+			i, err := strconv.Atoi(idstr)
+			if err != nil {
+				continue
+			}
+
+			id := int64(i)
+			if _, err := m.load(id); err != nil {
+				// TODO file exists at location but cannot load it
+				// should this return an error or load other files?
+				continue
+			}
+		}
+	}
+
+	return nil
+}
+
+// ReadAt reads data from memory maps starting from offset `off`
+func (m *Map) ReadAt(p []byte, off int64) (n int, err error) {
+	s := int64(len(p))
+	p = p[:0]
+
+	ps, err := m.ZReadAt(s, off)
+	if err != nil {
+		return 0, err
+	}
+
+	for _, r := range ps {
+		n += len(r)
+		p = append(p, r...)
+	}
+
+	return n, nil
+}
+
+// ZReadAt reads data from memory maps starting from offset `off`
+// ZReadAt returns a slice of slices from the memory map themselves.
+// Data gets read without memory copying but it can be unsafe at times.
+// Make sure that the memory map remains mapped while using this data.
+// For extended use, make a copy of this data or use the `ReadAt` method.
+func (m *Map) ZReadAt(sz, off int64) (ps [][]byte, err error) {
+	nfiles := sz / m.size
+	if off%m.size != 0 {
+		nfiles++
+	}
+
+	ps = make([][]byte, 0, nfiles)
+	sf, ef, so, eo := m.bounds(sz, off)
+
+	for i := sf; i <= ef; i++ {
+		var fso int64
+		var feo = m.size
+
+		if i == sf {
+			fso = so
+		}
+
+		if i == ef {
+			feo = eo
+		}
+
+		m.mutx.Lock()
+		f, err := m.load(i)
+		if err != nil {
+			m.mutx.Unlock()
+			return nil, err
+		}
+		m.mutx.Unlock()
+
+		d := f.Data[fso:feo]
+		ps = append(ps, d)
+	}
+
+	if err != nil {
+		return ps, err
+	}
+
+	return ps, nil
+}
+
+// WriteAt writes data to memory maps starting from offset `off`
+func (m *Map) WriteAt(p []byte, off int64) (n int, err error) {
+	sz := int64(len(p))
+	sf, ef, so, eo := m.bounds(sz, off)
+
+	for i := sf; i <= ef; i++ {
+		var fso int64
+		var feo = m.size
+
+		if i == sf {
+			fso = so
+		}
+
+		if i == ef {
+			feo = eo
+		}
+
+		m.mutx.Lock()
+		f, err := m.load(i)
+		if err != nil {
+			m.mutx.Unlock()
+			return n, err
+		}
+		m.mutx.Unlock()
+
+		ln := int(feo - fso)
+		copy(f.Data[fso:feo], p[n:n+ln])
+		n += ln
+	}
+
+	// check whether the file after last used file exists
+	// if not available load in a background goroutine
+	m.prealloc(ef + 1)
+
+	return n, nil
+}
+
+// Close closes all loaded memory maps
+func (m *Map) Close() (err error) {
+	for _, f := range m.Maps {
+		if err := f.Close(); err != nil {
+			return err
+		}
+
+	}
+	return nil
+}
+
+// load creates a memory map and adds it to the map.
+// make sure the mutex is locked before running this.
+func (m *Map) load(id int64) (f *memmap.Map, err error) {
+	if f, ok := m.Maps[id]; ok {
+		return f, nil
+	}
+
+	idstr := strconv.Itoa(int(id))
+	f, err = memmap.NewMap(m.path+idstr, m.size)
+	if err != nil {
+		return nil, err
+	}
+
+	// add new map
+	m.Maps[id] = f
+
+	return f, nil
+}
+
+func (m *Map) bounds(sz, off int64) (sf, ef, so, eo int64) {
+	end := off + sz
+
+	sf = off / m.size
+	so = off % m.size
+	ef = end / m.size
+	eo = end % m.size
+
+	if eo == 0 {
+		eo = m.size
+		ef--
+	}
+
+	return sf, ef, so, eo
+}
+
+// prealloc allocates a new file in a background go-routine.
+// This is extremely similar to `Load` except the background part.
+func (m *Map) prealloc(id int64) {
+	// fast path: file already exists
+	// RLocks costs lower than Locks
+	m.mutx.RLock()
+	_, ok := m.Maps[id]
+	m.mutx.RUnlock()
+
+	if ok {
+		return
+	}
+
+	m.mutx.Lock()
+	if _, ok := m.Maps[id]; ok {
+		m.mutx.Unlock()
+		return
+	}
+
+	go func() {
+		if _, err := m.load(id); err != nil {
+			// NOTE: failed to pre-allocate file.
+			// We can safely ignore this error.
+		}
+
+		m.mutx.Unlock()
+	}()
+}
