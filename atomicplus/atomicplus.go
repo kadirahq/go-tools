58ec71274beba914609ddde5005c36db80ea9ad4
Muhammed Thanish <mnmtanish@gmail.com>
2015-09-28 19:18:43 +0530

    Convert the pointer only once

--- atomicplus/atomicplus.go
+++ atomicplus/atomicplus.go
@@ -7,17 +7,16 @@ import (
 
 // AddFloat64 atomically adds a float64 to a float64 and returns new value
 func AddFloat64(addr *float64, delta float64) (new float64) {
-
+	var old float64
 	convertedAddr := (*int64)(unsafe.Pointer(addr))
+	convertedOld := (*int64)(unsafe.Pointer(&old))
+	convertedNew := (*int64)(unsafe.Pointer(&new))
 
 	for {
-		old := *addr
+		old = *addr
 		new = old + delta
 
-		convertedOld := *(*int64)(unsafe.Pointer(&old))
-		convertedNew := *(*int64)(unsafe.Pointer(&new))
-
-		if atomic.CompareAndSwapInt64(convertedAddr, convertedOld, convertedNew) {
+		if atomic.CompareAndSwapInt64(convertedAddr, *convertedOld, *convertedNew) {
 			break
 		}
 	}
