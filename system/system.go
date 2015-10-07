7621f5d3c45f3ca8a857105f0f9f6be413d42781
Muhammed Thanish <mnmtanish@gmail.com>
2015-10-07 10:20:08 +0530

    Export cpu usage with a function

--- system/system.go
+++ system/system.go
@@ -5,20 +5,26 @@ import "C"
 import "time"
 
 var (
-	// PCPU has cpu usage as a percentage
+	// pcpu has cpu usage as a percentage
 	// this value is updated every second
-	PCPU float64
+	pcpu float64
 )
 
 func init() {
-	go setPCPU()
+	go setpcpu()
 }
 
-func setPCPU() {
+func setpcpu() {
 	for {
 		ticks := C.clock()
 		time.Sleep(time.Second)
 		clock := float64(C.clock()-ticks) / float64(C.CLOCKS_PER_SEC)
-		PCPU = 100 * clock
+		pcpu = 100 * clock
 	}
+}
+
+// PCPU returns the total cpu usage as a percentage. The usage value is updated
+// in the background every second. Usage is claculated by counting cpu ticks.
+func PCPU() float64 {
+	return pcpu
 }
