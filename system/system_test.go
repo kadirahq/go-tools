e71f8b3ee9d87d9664aa6378460ca38a8faff6f1
Muhammed Thanish <mnmtanish@gmail.com>
2015-10-08 11:54:50 +0530

    Reduce test time

--- system/system_test.go
+++ system/system_test.go
@@ -1,14 +1,19 @@
 package system
 
 import (
-	"fmt"
 	"testing"
 	"time"
 )
 
 func TestPCPU(t *testing.T) {
-	for i := 0; i < 5; i++ {
-		time.Sleep(time.Second)
-		fmt.Printf("PCPU: %.3f%%\n", PCPU)
+	if PCPU() != 0 {
+		t.Fatal("wrong initial value")
+	}
+
+	// wait at least 2 second
+	time.Sleep(2 * time.Second)
+
+	if PCPU() <= 0 {
+		t.Fatal("must have a valid value")
 	}
 }
