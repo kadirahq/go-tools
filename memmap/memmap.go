bf356fff69c8f48d24c06254630a88665a2bafb2
Muhammed Thanish <mnmtanish@gmail.com>
2015-10-12 15:12:27 +0530

    Add MapFile method

--- memmap/memmap.go
+++ memmap/memmap.go
@@ -30,14 +30,14 @@ var (
 // and easy to use api. The Map should be unmapped when not in use.
 type Map struct {
 	Data []byte
-	file *os.File
 	hlen uintptr
 	hadr uintptr
 }
 
-// NewMap creates a new memory map struct. This struct contains map data and
-// other information needed to manipulate the memory map later.
-func NewMap(path string, size int64) (m *Map, err error) {
+// New creates a new memory map struct on given path
+// A new file will be created on given path if necessary.
+// The file will be truncated to given size if it's empty.
+func New(path string, size int64) (m *Map, err error) {
 	if size == 0 {
 		return nil, ErrZeroSz
 	}
@@ -47,33 +47,48 @@ func NewMap(path string, size int64) (m *Map, err error) {
 		return nil, err
 	}
 
-	info, err := file.Stat()
+	// don't need this
+	defer file.Close()
+
+	m, err = MapFile(file, size)
 	if err != nil {
-		file.Close()
 		return nil, err
 	}
 
-	fd := file.Fd()
-	sz := info.Size()
+	return m, nil
+}
 
-	if sz != size {
+// MapFile creates a new memory map struct from an os.File
+// The file will be truncated to given size if it's empty.
+func MapFile(file *os.File, size int64) (m *Map, err error) {
+	if size == 0 {
+		return nil, ErrZeroSz
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if sz := info.Size(); sz != size {
 		if sz != 0 {
-			file.Close()
+			// file already exists with different size
+			// this can be caused by corrupted files
 			return nil, ErrBadSz
 		}
 
+		// If the file size if zero, it should be a new
+		// segment file. Truncate it to required size.
 		if err := file.Truncate(size); err != nil {
-			file.Close()
 			return nil, err
 		}
 
 		sz = size
 	}
 
-	var from int64
-	data, err := syscall.Mmap(int(fd), from, int(sz), mprot, mflag)
+	fd := file.Fd()
+	data, err := syscall.Mmap(int(fd), 0, int(size), mprot, mflag)
 	if err != nil {
-		file.Close()
 		return nil, err
 	}
 
@@ -82,7 +97,6 @@ func NewMap(path string, size int64) (m *Map, err error) {
 
 	m = &Map{
 		Data: data,
-		file: file,
 		hadr: head.Data,
 		hlen: uintptr(head.Len),
 	}
