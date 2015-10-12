2dd05efb3dd0037c85ffd1566cb8075c5819dbcd
Muhammed Thanish <mnmtanish@gmail.com>
2015-10-12 15:15:49 +0530

    Add fs package

---
+++ fs/fs.go
@@ -0,0 +1,19 @@
+package fs
+
+// Syncer interface provides a Sync method which will
+// permenantly store data to a device (eg. hard-disk)
+type Syncer interface {
+	Sync() (err error)
+}
+
+// SlicerAt interface provides an SliceAt method which will
+// fetch a slice of bytes from an in-memory data storage.
+type SlicerAt interface {
+	SliceAt(sz, off int64) (p []byte, err error)
+}
+
+// Slicer interface provides a Slice method which will
+// fetch a slice of bytes from an in-memory data storage.
+type Slicer interface {
+	Slice(sz int64) (p []byte, err error)
+}
